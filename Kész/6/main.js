caf762e6b956be445380ce99ca093550c59677d7
Avarka <vadavar7@gmail.com>
2021-11-03 19:05:14 +0100

    feat(kész-6): Correctly completed task 6

--- Kész/6/main.js
+++ Kész/6/main.js
@@ -1,13 +1,14 @@
 const table = document.getElementById("game");
 let lastClicked = 0;
 let time = 0;
+let difficulty = 7;
 let interval;
 
 const resetGame = () => {
     table.innerHTML = "";
-    for (let y = 0; y < 10; y++){
+    for (let y = 0; y < 10; y++) {
         const tableRow = document.createElement("tr");
-        for (let x = 0; x < 10; x++){
+        for (let x = 0; x < 10; x++) {
             const tableCol = document.createElement("td");
             tableCol.id = x + "/" + y;
             tableCol.innerText = "";
@@ -19,53 +20,65 @@ const resetGame = () => {
     clearInterval(interval);
     time = 0;
     lastClicked = 0;
-}
-
-
+};
 
 const clicked = event => {
-    console.log(lastClicked);
-    if (Number(event.target.innerText) === (lastClicked + 1)){
+    if (Number(event.target.innerText) === lastClicked + 1) {
         lastClicked++;
+        console.log(lastClicked);
+        if (lastClicked === 1) {
+            for (let element of document.querySelectorAll(".selected")) {
+                element.classList.add("hidden");
+            }
+        }
         event.target.classList.add("success");
-        if (lastClicked === 7) {
+        if (lastClicked === difficulty) {
             setTimeout(() => {
                 alert(`Gratulálunk sikerült hiba nélkül megcsinálni a feladatot. ${time} másodpercbe telt.`);
-            }, 100)
+
+                clearInterval(interval);
+                time = 0;
+                lastClicked = 0;
+            }, 100);
+        }
+    } else {
+        event.target.classList.add("fail");
+        for (let element of document.querySelectorAll(".selected")) {
+            element.removeEventListener("click", clicked);
+        }
+        setTimeout(() => {
+            alert("A feladatot nem sikerült teljesíteni. Próbálja újra!");
+
             clearInterval(interval);
             time = 0;
             lastClicked = 0;
-        }
-    }else{
-        event.target.classList.add("fail");
-        for(let element of document.querySelectorAll(".selected")) {
-            element.removeEventListener("click", clicked);
-        }
-        clearInterval(interval);
-        time = 0;
-        lastClicked = 0;
-        setTimeout(() => {
-            alert("A feladatot nem sikerült teljesíteni. Próbálja újra!")
-        }, 100)
+        }, 100);
     }
 };
 
 const start = () => {
     resetGame();
-    interval = setInterval(()=>{time++}, 1000);
+    interval = setInterval(() => {
+        console.log("int");
+        time++;
+    }, 1000);
 
-    for (let i = 0; i < 7; i++){
+    for (let i = 0; i < difficulty; i++) {
         const x = Math.floor(Math.random() * 10);
         const y = Math.floor(Math.random() * 10);
-        if (document.getElementById(x + "/" + y).innerText === ""){
+        if (document.getElementById(x + "/" + y).innerText === "") {
             const selected = document.getElementById(x + "/" + y);
-            selected.innerText=(i + 1).toString();
+            selected.innerText = (i + 1).toString();
             selected.classList.add("selected");
-            selected.addEventListener("click", clicked)
-        }
-        else
-            i--;
+            selected.addEventListener("click", clicked);
+        } else i--;
     }
+};
+
+const changeDifficulty = () => {
+    const newDiff = document.querySelector("#difficulty");
+    difficulty = Number(newDiff.value);
+    resetGame();
 };
 
 document.getElementById("start").addEventListener("click", start);
